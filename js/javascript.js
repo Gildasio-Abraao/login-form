dd17d454ad4aeb2b8a9c7c959b702f02fe9fe38a
Gildasio Abraao <gildasio.abraao3@gmail.com>
2023-01-31 18:40:51 -0300

    refactor: Refatorando todo o código

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,5 @@
-function button_cadastro()  {
-    let back = document.querySelector('.back_cadastro');
-    let login = document.querySelector('#tela_login');
+function changeViewScreen()  {
+    const screens = document.querySelectorAll('.screen');
 
-    back.classList.toggle('animation_cadastro');
-    login.classList.toggle('index');
-}
-function button_login()  {
-  let backLogin = document.querySelector('.back_login');
-  let signup = document.querySelector('#tela_login');
-  
-  backLogin.classList.toggle('animation_login');
-  signup.classList.toggle('index');
+    screens.forEach(screen => screen.classList.toggle('display-none'));
 }
